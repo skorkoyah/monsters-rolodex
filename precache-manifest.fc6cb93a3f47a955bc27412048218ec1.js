debb53e861833b0dc9073dab7b83b11a654df407
skorkoyah <skorkoyah29@gmail.com>
2020-02-12 08:34:42 -0500

    Updates

---
+++ precache-manifest.fc6cb93a3f47a955bc27412048218ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27d3d55ab0df20247cdecb73c0b18c5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b1d14f2facd035cafd1",
+    "url": "/monsters-rolodex/static/css/main.26737a94.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b1d14f2facd035cafd1",
+    "url": "/monsters-rolodex/static/js/main.b5440141.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
